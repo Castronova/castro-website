7eec1206ca8c5efcf442edd1fa259d6af4d0bd41
Tony Castronova <castronova.anthony@gmail.com>
2014-10-16 15:53:04 -0600

    fixed deployment

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,6 +1,4 @@
 set :application, "hello"
-<<<<<<< HEAD
-
 set :repository, "git@github.com:Castronova/castro-website.git"
 set :domain, "castro@castro-ws.bluezone.usu.edu"
 set :deploy_to, "/var/www/hello"
@@ -8,9 +6,3 @@ set :deploy_to, "/var/www/hello"
 set :user, "castro"
 set :revision, "origin/master"
 
-
-=======
-set :repository, "ssh://git@github.com:Castronova/castro-website.git"
-set :domain, "castro@castro-ws.bluezone.usu.edu"
-set :deploy_to, "/home/castro/Documents/website/hello"
->>>>>>> 8cff901cf3638cad844c9faedd9f8f2ad131dea8
