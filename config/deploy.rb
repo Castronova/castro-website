30e035df8d482284321a8c622c998ed0a394988d
Tony Castronova <castronova.anthony@gmail.com>
2014-10-16 17:03:49 -0600

    edits to deployment script"

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,3 +6,16 @@ set :deploy_to, "/var/www/hello"
 set :user, "castro"
 set :revision, "origin/master"
 
+namespace :vlad do
+  set :app_command, "/etc/init.d/apache2"
+ 
+  desc 'Restart Passenger'
+  remote_task :start_app, :roles => :app do
+    run "touch #{current_release}/tmp/restart.txt"
+  end
+  
+  desc 'Restarts the apache servers'
+  remote_task :start_web, :roles => :app do
+    run "sudo #{app_command} restart"
+  end
+end
