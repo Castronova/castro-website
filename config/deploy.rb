f61de2a97a06c11495cebc5cd156aede1eedc1cd
Tony Castronova <castronova.anthony@gmail.com>
2014-10-16 12:54:56 -0600

    initial commit

---
+++ config/deploy.rb
@@ -0,0 +1,4 @@
+set :application, "hello"
+set :repository, "ssh://git@github.com:Castronova/castro-website.git"
+set :domain, "castro@castro-ws.bluezone.usu.edu"
+set :deploy_to, "/home/castro/Documents/website/hello"
