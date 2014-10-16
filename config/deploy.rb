a1e5020178ec9572e015c655067afe19d5ba86c9
Tony Castronova <castronova.anthony@gmail.com>
2014-10-16 15:47:29 -0600

    replaced files with nesta demo

---
+++ config/deploy.rb
@@ -0,0 +1,10 @@
+set :application, "hello"
+
+set :repository, "git@github.com:Castronova/castro-website.git"
+set :domain, "castro@castro-ws.bluezone.usu.edu"
+set :deploy_to, "/var/www/hello"
+
+set :user, "castro"
+set :revision, "origin/master"
+
+
